248276d515da685b9fc2a75195c47b27fbd743bf
HaiderMalik <haidermalik504@gmail.com>
2017-12-15 20:17:40 +0500

    nodejs demo

---
+++ hello-world.js
@@ -0,0 +1,21 @@
+setTimeout(function(){
+  console.log('Fetched Proucts');
+},2000);
+
+setTimeout(function () {
+    console.log('Fetched Categories');
+}, 3000);
+
+//long running task
+console.log('Reading and Loading other stuff!!');
+console.log('Business Logic!!');
+
+// echo('Hello');
+// sleep(2);
+// echo('world!!');
+
+
+//dont need to sleep in node.js
+
+
+
